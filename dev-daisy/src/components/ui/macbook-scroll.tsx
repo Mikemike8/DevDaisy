832b9148e9478719d8b99b462ec230281db46b2f
Michael Harris <158084149+Mikemike8@users.noreply.github.com>
2025-08-28 10:27:57 -0500

    mm,

--- dev-daisy/src/components/ui/macbook-scroll.tsx
+++ dev-daisy/src/components/ui/macbook-scroll.tsx
@@ -56,7 +56,7 @@ useEffect(() => {
 }, []);
 
 // Offset for text
-const textOffset = isMobile ? 150 : isTablet ? 50 : 120; // move text up by 50px on all devices
+const textOffset = isMobile ? 300 : isTablet ? 50 : 120; // move text up by 50px on all devices
 
 // Offset for Macbook image
 const macbookOffset = isMobile ? 1150 : isTablet ? 170 : 400;
