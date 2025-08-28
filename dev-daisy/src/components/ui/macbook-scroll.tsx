510b9d3feeae2174a0daf2fc24c0eaf9425b9dd7
Michael Harris <158084149+Mikemike8@users.noreply.github.com>
2025-08-28 09:55:02 -0500

    ds

--- dev-daisy/src/components/ui/macbook-scroll.tsx
+++ dev-daisy/src/components/ui/macbook-scroll.tsx
@@ -82,7 +82,7 @@ const textOpacity = useTransform(scrollYProgress, [0, 0.2], [1, 0]);
     translateY: useTransform(textTransform, (y) => y - textOffset),
     opacity: textOpacity,
   }}
-  className="mb-20 -mt-16 w-screen text-center text-orange-600 dark:text-white font-bold leading-snug sm:leading-tight 
+  className="mb-10 -mt-4 w-screen text-center text-orange-600 dark:text-white font-bold leading-snug sm:leading-tight 
              text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl"
 >
   {title || (
